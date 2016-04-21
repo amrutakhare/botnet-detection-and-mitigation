ca1f37fb42962dc7978643bd21d952c6839cf4c4
Amruta <amrutapkhare@gmail.com>
2016-04-20 20:03:44 -0700

    Updating output format.

--- botnet-analysis-and-mitigation/detectors/TcpSynFloodingDetector.py
+++ botnet-analysis-and-mitigation/detectors/TcpSynFloodingDetector.py
@@ -1,8 +1,10 @@
 # TCP SYN-ACK Packet Flooding Detector
 def detectTCPSynFlooding(pcapRecords, thresholdPercentage, windowSize):		
-	for index, record in enumerate(pcapRecords):
+    instances = 0
+    for index, record in enumerate(pcapRecords):
 			if isTCPSynPacket(record.protocol, record.info):		
-				checkWindowFrame(index, pcapRecords, windowSize, thresholdPercentage)
+				instances = instances + checkWindowFrame(index, pcapRecords, windowSize, thresholdPercentage)
+    print '\nTCP SYN Flooding Instances = ' + str(instances)
 				
 				
 # Check how many TCP SYN Packets are sent within the WindowSize.				
@@ -15,6 +17,8 @@ def checkWindowFrame(index, pcapRecords, windowSize, thresholdPercentage):
 			windowStart = windowStart + 1
 	if (countInWindow*100/windowSize) >= thresholdPercentage and index + windowSize < len(pcapRecords):
 			print 'Detected TCP SYN Flooding between ' + str(pcapRecords[index].timestamp) + ' and ' + str(pcapRecords[index + windowSize].timestamp) + ': ' + str(countInWindow*100/windowSize) + '%'
+			return 1
+	return 0
 
 
 # Check if the current packet contributes to the TCP SYN Flooding
